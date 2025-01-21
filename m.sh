dbfd255acc18c178a06c40c42b4e4380158a95e5
Brainzeus <89934607+Brainzeus@users.noreply.github.com>
2025-01-22 00:39:57 +0200

    Add files via upload

---
+++ m.sh
@@ -0,0 +1,2 @@
+./SRBMiner-MULTI --algorithm randomx --pool de.zephyr.herominers.com:1123 --wallet ZEPHYR39UVMDvsXUcxMyGJ94gY181ZKK1XgALsfP7xNjHJthv36bZyJ9Wh3oxcjShLBsGNoyhBF1s9mweR3n2Ei3ASjTjpnxhwz3X --password AERS
+pause
