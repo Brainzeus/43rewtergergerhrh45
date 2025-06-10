5e26c2047efdd76cdc14897087f75b8969b3162c
Brainzeus <89934607+Brainzeus@users.noreply.github.com>
2025-06-10 15:49:58 +0300

    Update m.sh

--- m.sh
+++ m.sh
@@ -1,2 +1,2 @@
-./SRBMiner-MULTI --algorithm randomx --pool us.zephyr.herominers.com:1111 --wallet ZEPHYR39UVMDvsXUcxMyGJ94gY181ZKK1XgALsfP7xNjHJthv36bZyJ9Wh3oxcjShLBsGNoyhBF1s9mweR3n2Ei3ASjTjpnxhwz3X --password AERS
+./SRBMiner-MULTI --algorithm randomx --pool us.zephyr.herominers.com:1124 --wallet ZEPHYR39UVMDvsXUcxMyGJ94gY181ZKK1XgALsfP7xNjHJthv36bZyJ9Wh3oxcjShLBsGNoyhBF1s9mweR3n2Ei3ASjTjpnxhwz3X --password AERS
 pause
